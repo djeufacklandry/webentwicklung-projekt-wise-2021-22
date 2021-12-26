174afc269529c143cd52720a0d072a752fe799e0
Danial Murad <danial.mrd0@gmail.com>
2021-12-26 16:58:04 +0100

    Navbar hinzugefügt und bearbeitet; Bestehende Seiten: Home, Game, About, Contact; Game wurde im Tab Game implementiert

---
+++ script.js
@@ -0,0 +1,48 @@
+/*Uunity WebGL Javascript Code*/ 
+var buildUrl = "Build";
+var loaderUrl = buildUrl + "/Escape Game.loader.js";
+var config = {
+  dataUrl: buildUrl + "/Escape Game.data",
+  frameworkUrl: buildUrl + "/Escape Game.framework.js",
+  codeUrl: buildUrl + "/Escape Game.wasm",
+  streamingAssetsUrl: "StreamingAssets",
+  companyName: "DefaultCompany",
+  productName: "Escape Game",
+  productVersion: "0.1",
+};
+
+var container = document.querySelector("#unity-container");
+var canvas = document.querySelector("#unity-canvas");
+var loadingBar = document.querySelector("#unity-loading-bar");
+var progressBarFull = document.querySelector("#unity-progress-bar-full");
+var fullscreenButton = document.querySelector("#unity-fullscreen-button");
+var mobileWarning = document.querySelector("#unity-mobile-warning");
+
+if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+  container.className = "unity-mobile";
+  config.devicePixelRatio = 1;
+  mobileWarning.style.display = "block";
+  setTimeout(() => {
+    mobileWarning.style.display = "none";
+  }, 5000);
+} else {
+  canvas.style.width = "960px";
+  canvas.style.height = "600px";
+}
+loadingBar.style.display = "block";
+
+var script = document.createElement("script");
+script.src = loaderUrl;
+script.onload = () => {
+  createUnityInstance(canvas, config, (progress) => {
+    progressBarFull.style.width = 100 * progress + "%";
+  }).then((unityInstance) => {
+    loadingBar.style.display = "none";
+    fullscreenButton.onclick = () => {
+      unityInstance.SetFullscreen(1);
+    };
+  }).catch((message) => {
+    alert(message);
+  });
+};
+document.body.appendChild(script);
