ce4a92d673056d0c21ed39a0786aec8c63912653
Danial Murad <danial.mrd0@gmail.com>
2022-01-06 13:13:33 +0100

    inline-style remove, added styles to css.style

--- script.js
+++ script.js
@@ -1,48 +1,10 @@
-/*Unity Game - Java Script*/
-var buildUrl = "Build";
-var loaderUrl = buildUrl + "/Maze Runner Vers. 1.01.loader.js";
-var config = {
-  dataUrl: buildUrl + "/Maze Runner Vers. 1.01.data",
-  frameworkUrl: buildUrl + "/Maze Runner Vers. 1.01.framework.js",
-  codeUrl: buildUrl + "/Maze Runner Vers. 1.01.wasm",
-  streamingAssetsUrl: "StreamingAssets",
-  companyName: "DanDev",
-  productName: "Maze Runner",
-  productVersion: "1.01",
-};
+//defer als attrbute beim laden vom script
+// läuft erst wenn js geladen ist
+/*
+client height
+get höhe
+event handler 
+pfeil animation mit css (wenn noch zeit ist)
 
-var container = document.querySelector("#unity-container");
-var canvas = document.querySelector("#unity-canvas");
-var loadingBar = document.querySelector("#unity-loading-bar");
-var progressBarFull = document.querySelector("#unity-progress-bar-full");
-var fullscreenButton = document.querySelector("#unity-fullscreen-button");
-var mobileWarning = document.querySelector("#unity-mobile-warning");
 
-if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
-  container.className = "unity-mobile";
-  config.devicePixelRatio = 1;
-  mobileWarning.style.display = "block";
-  setTimeout(() => {
-    mobileWarning.style.display = "none";
-  }, 5000);
-} else {
-  canvas.style.width = "960px";
-  canvas.style.height = "600px";
-}
-loadingBar.style.display = "block";
-
-var script = document.createElement("script");
-script.src = loaderUrl;
-script.onload = () => {
-  createUnityInstance(canvas, config, (progress) => {
-    progressBarFull.style.width = 100 * progress + "%";
-  }).then((unityInstance) => {
-    loadingBar.style.display = "none";
-    fullscreenButton.onclick = () => {
-      unityInstance.SetFullscreen(1);
-    };
-  }).catch((message) => {
-    alert(message);
-  });
-};
-document.body.appendChild(script);
+*/ 
